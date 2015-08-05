98256017e9b6e5220bb1932055e9deb261f76118
Ace <ace.github@gmail.com>
2015-08-05 11:33:02 -0700

    Moved data to Mongo...Meteor is fucking cool

--- client/views/posts_list.js
+++ client/views/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [{
-    title: 'This is a test post',
-    author: 'Ace Eddleman',
-    url: 'http://www.testing123.com'
-}, {
-    title: 'Another test post',
-    author: 'Joe Blow',
-    url: 'http://www.testing456.com'
-},{
-    title: 'Yep, another test',
-    author: 'Jane Blow',
-    url: 'http://www.testing789.com'
-}
-];
-
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
